c1570f35f63e999334210aaed06174d7406d9b78
GovindJoshi <xgovindjoshi@gmail.com>
2026-06-05 10:04:22 -0500

    pandafile

---
+++ src/panda_module_test.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import pandas as pd
+
+csv_path = Path(r"C:\Users\bhart\Downloads\urls2.txt")
+
+df = pd.read_csv(csv_path, header=None, names=["url"])
+
+df = df.astype({"url": str})
+
+url_counts = df["url"].value_counts()
+
+max_url = url_counts.idxmax()
+max_count = url_counts.max()
+print(f"Most frequent URL: {max_url}")
+print(f"Occurrence count: {max_count}")
