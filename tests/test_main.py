94b730840600b48bfce19671f246293bd2bc295d
GovindJoshi <xgovindjoshi@gmail.com>
2026-06-05 11:26:56 -0500

    sub test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
-from src.main import add
+from src.main import add,sub
 
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+    
+def test_sub():
+    assert sub(5, 3) == 2
+    assert sub(1, 1) == 0
+    assert sub(0, 0) == 0
